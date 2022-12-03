75979d4644e12ae06866967600a239be10a08f06
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-12-02 23:21:14 -0300

    terminando a tela de dados

--- Styles/PaginaDadosStyle.tsx
+++ Styles/PaginaDadosStyle.tsx
@@ -31,11 +31,63 @@ const keyboardHide = StyleSheet.create({
     name: {
         top: '60%', fontSize: 20, fontWeight: 'bold'
     },
-    itensContainer:{
-        width:'100%',
-        height:'60%',
-        top:150,
-        alignItems:'center'
+    alterImageContainer: {
+        width: 30,
+        height: 30,
+        backgroundColor: 'white',
+        borderRadius: 100,
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: '80%',
+        left: '58%',
+        borderWidth: 2,
+        borderColor: '#979696'
+    },
+    itensContainer: {
+        width: '100%',
+        height: '60%',
+        top: 150,
+        alignItems: 'center'
+    },
+    gerenciarContaContainer: {
+        width: '90%',
+        height: 50,
+        borderRadius: 12,
+        borderWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    gerenciarContaImage: {
+        width: 40,
+        height: 40,
+        left: 10,
+    },
+    gerenciarContaText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        left: 20
+    },
+    iconSeta: {
+        width: 13,
+        height: 15,
+        position: 'absolute',
+        top: '35%',
+        left: '93%',
+        tintColor: '#787777'
+    },
+    sairTextContainer: {
+        width: '70%',
+        height: '100%',
+        left: 20
+    },
+    sairText1: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    sairText2: {
+        fontSize: 13,
+        color: '#787777'
     },
 })
 
--- Views/PaginasUsuario/PaginaDados.tsx
+++ Views/PaginasUsuario/PaginaDados.tsx
@@ -1,27 +1,47 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import DadosItens from "../../Components/DadosItens";
 import PaginaDadosStyle from "../../Styles/PaginaDadosStyle"
 
 
-export default function PaginaDados(props){
-return(
-    <View style={PaginaDadosStyle.keyboardHide.body}>
-        <View style={PaginaDadosStyle.keyboardHide.head}>
-        <View style={PaginaDadosStyle.keyboardHide.imageContainer}>
-            <Image style={PaginaDadosStyle.keyboardHide.image} resizeMode={'stretch'} source={props.usuario.perfil.Image}/>
-        </View>
-        <Text style={PaginaDadosStyle.keyboardHide.name}>{props.usuario.perfil.nome}</Text>
-        </View>
-        <View style={PaginaDadosStyle.keyboardHide.itensContainer}>
-        <DadosItens border={true} image={require('../../assets/card1.png')} titulo={'CPF'} text={props.usuario.perfil.CPF}/>
-        <DadosItens border={true} image={require('../../assets/card2.png')} titulo={'Cartão de vacina'} text={props.usuario.perfil.CartaoDeVacina}/>
-        <DadosItens border={false} image={require('../../assets/card3.png')} titulo={'Data de Nascimento'} text={props.usuario.perfil.DataDeNascimento}/>
-        <Text>{'\n'}</Text>
-        <View style={{width:'90%', height:50, borderRadius:12,  borderWidth:1}}>
+export default function PaginaDados(props) {
+    return (
+        <View style={PaginaDadosStyle.keyboardHide.body}>
+            <View style={PaginaDadosStyle.keyboardHide.head}>
+                <View style={PaginaDadosStyle.keyboardHide.imageContainer}>
+                    <Image style={PaginaDadosStyle.keyboardHide.image} resizeMode={'stretch'} source={props.usuario.perfil.Image} />
+                </View>
+                <Text style={PaginaDadosStyle.keyboardHide.name}>{props.usuario.perfil.nome}</Text>
+                <TouchableOpacity style={PaginaDadosStyle.keyboardHide.alterImageContainer} >
+                    <Image style={{ width: '80%', height: '80%' }} resizeMode={'contain'} source={require('../../assets/pencil.png')} />
+                </TouchableOpacity>
+            </View>
+            <View style={PaginaDadosStyle.keyboardHide.itensContainer}>
+                <DadosItens border={true} image={require('../../assets/card1.png')} titulo={'CPF'} text={props.usuario.perfil.CPF} />
+                <DadosItens border={true} image={require('../../assets/card2.png')} titulo={'Cartão de vacina'} text={props.usuario.perfil.CartaoDeVacina} />
+                <DadosItens border={false} image={require('../../assets/card3.png')} titulo={'Data de Nascimento'} text={props.usuario.perfil.DataDeNascimento} />
+                <Text>{'\n'}</Text>
 
+                <TouchableOpacity style={PaginaDadosStyle.keyboardHide.gerenciarContaContainer}>
+                    <>
+                        <Image style={PaginaDadosStyle.keyboardHide.gerenciarContaImage} source={require('../../assets/group.png')} />
+                        <Text style={PaginaDadosStyle.keyboardHide.gerenciarContaText}>{'Gerenciar Conta'}</Text>
+                        <Image style={PaginaDadosStyle.keyboardHide.iconSeta} resizeMode={'stretch'} source={require('../../assets/_.png')} />
+                    </>
+                </TouchableOpacity>
+                <Text>{'\n'}</Text>
+                <TouchableOpacity style={PaginaDadosStyle.keyboardHide.gerenciarContaContainer}>
+                    <>
+                        <Image style={PaginaDadosStyle.keyboardHide.gerenciarContaImage} source={require('../../assets/exit.png')} />
+                        <View style={PaginaDadosStyle.keyboardHide.sairTextContainer}>
+                            <Text style={PaginaDadosStyle.keyboardHide.sairText1}>{'Sair'}</Text>
+                            <Text style={PaginaDadosStyle.keyboardHide.sairText2}>{'Desconectar sua conta desse aparrelho'}</Text>
+                        </View>
+
+                        <Image style={PaginaDadosStyle.keyboardHide.iconSeta} resizeMode={'stretch'} source={require('../../assets/_.png')} />
+                    </>
+                </TouchableOpacity>
+            </View>
         </View>
-        </View>
-    </View>
-);
+    );
 }
